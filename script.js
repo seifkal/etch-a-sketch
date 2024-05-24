9b96eeadf74fe78f2e76f39bcec6b92534e3e0aa
pepperfloat <saifkall5@gmail.com>
2024-05-24 12:18:25 +0300

    made the size slider functional

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 let container = document.querySelector(".container");
-for(let i=0;i<16;i++){
-    let boxcontainer=document.createElement("div");
-    boxcontainer.classList="boxcontainer";
-    for(let x=0;x<16;x++){
-        let box=document.createElement("div");
-        box.classList="box";
-        box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor="#393e46";
-        });
-        boxcontainer.appendChild(box);
-    }
-    container.appendChild(boxcontainer)
-};
+let ismousedown=false;
 let slider=document.querySelector(".slider");
-slider.value=1;
-function sketchgrid(){
-    for(let i=0;i<16;i++){
+function pixelcalc(n) {
+    const values = [48, 24, 16, 12];
+    return values[n - 1];
+};
+function sketchgrid(size){
+    let pixelsize=pixelcalc(slider.value)+"px";
+    for(let i=0;i<(size*8);i++){
         let boxcontainer=document.createElement("div");
+        boxcontainer.setAttribute("draggable", "false");
         boxcontainer.classList="boxcontainer";
-        for(let x=0;x<16;x++){
+        for(let x=0;x<(size*8);x++){
             let box=document.createElement("div");
             box.classList="box";
-            box.addEventListener("mouseenter", () => {
+            boxcontainer.setAttribute("draggable", "false");
+            box.style.width=pixelsize;
+            box.style.height=pixelsize;
+            box.addEventListener("mousedown", () => {
                 box.style.backgroundColor="#393e46";
+            });
+            box.addEventListener("mouseenter", () => {
+                if(ismousedown==true){
+                    box.style.backgroundColor="#393e46";
+                };
             });
             boxcontainer.appendChild(box);
         }
         container.appendChild(boxcontainer)
     };
-}
+};
+function deleteboxes() {
+    const containers = document.querySelectorAll('.boxcontainer');
+    containers.forEach(container => {
+        container.parentNode.removeChild(container);
+    });
+};
+container.addEventListener("mousedown", () => {
+    ismousedown=true;
+});
+container.addEventListener("mouseup", () => {
+    ismousedown=false;
+});
+slider.addEventListener("input", () => {
+    deleteboxes();
+    sketchgrid(slider.value);
+})
+slider.value=2;
+sketchgrid(2);
 //https://palettes.shecodes.io/palettes/1435
+//1 8boxes 48px
+//2 16boxes 24px
+//3 24boxes 16px
+//4 32boxes 12px

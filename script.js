a90f9bf0522112bc52070479b53061d1fdc9fbab
pepperfloat <saifkall5@gmail.com>
2024-05-24 07:43:25 +0300

    assigned colors,header, and added a slider(still not functional)

--- script.js
+++ script.js
@@ -5,10 +5,28 @@ for(let i=0;i<16;i++){
     for(let x=0;x<16;x++){
         let box=document.createElement("div");
         box.classList="box";
-        box.addEventListener("click", () => {
-            box.style.backgroundColor="black";
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor="#393e46";
         });
         boxcontainer.appendChild(box);
     }
     container.appendChild(boxcontainer)
+};
+let slider=document.querySelector(".slider");
+slider.value=1;
+function sketchgrid(){
+    for(let i=0;i<16;i++){
+        let boxcontainer=document.createElement("div");
+        boxcontainer.classList="boxcontainer";
+        for(let x=0;x<16;x++){
+            let box=document.createElement("div");
+            box.classList="box";
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor="#393e46";
+            });
+            boxcontainer.appendChild(box);
+        }
+        container.appendChild(boxcontainer)
+    };
 }
+//https://palettes.shecodes.io/palettes/1435
